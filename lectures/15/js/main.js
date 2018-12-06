2b88d578c74c7f469d8925608c1ae993d70c23c1
YasirAmmar56 <44520152+YasirAmmar56@users.noreply.github.com>
2018-12-06 14:34:48 +0500

    Update main.js
    mock exam

---
+++ lectures/15/js/main.js
@@ -0,0 +1,25 @@
+var account = [
+ 	{
+    	Name:"Yasir Ammar",
+		accountbalance:99999,
+		currency:"PKR",
+		IBN:12345,
+		temp:"sdsadsadsa"
+	}
+]
+document.getElementById("title").innerHTML=account[0].Name;
+document.getElementById("balance").innerHTML=account[0].accountbalance;
+document.getElementById("currency").innerHTML=account[0].currency;
+document.getElementById("IBAN").innerHTML=account[0].IBN;
+function Deposite(event,val) {
+if(event.keycode==13)
+{
+   
+
+
+}
+
+
+
+
+}
